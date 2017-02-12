60423f44db241de093f8f947b1950f26a20f12df
saitonanae <saito-nanae@av.avex.co.jp>
2017-02-12 06:09:18 +0000

    04_実習_ユーザ認証機能の作成2終了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  
-  config.action_mailer.default_url_options = { host: 'elites08-saitonanae' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -40,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'elites08-saitonanae.c9users.io' }
+  config.action_mailer.delivery_method = :letter_opener_web
 end
